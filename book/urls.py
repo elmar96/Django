c34d0bd86ef4339f8244f1db9e24ce283a2c0987
elmar <elmarbek42@gmail.com>
2022-05-22 23:24:11 +0600

    home_work_4

--- book/urls.py
+++ book/urls.py
@@ -4,6 +4,28 @@ from . import views
 app_name = "books_url"
 urlpatterns = [
     path('books/', views.all_books, name="books_all_url"),
+    path('books/latest/', views.all_books_latest, name="latest"),
+    path('books/drama/', views.all_books_drama, name="drama"),
+    path('books/fantasy/', views.all_books_fantasy, name="frama"),
+    path('books/thriller/', views.all_books_thriller, name="thriller"),
+    path('books/romance/', views.all_books_romance, name="Romance"),
+    path('books/westerns/', views.all_books_westerns, name="westerns"),
+    path('books/literary_fiction/', views.all_books_literary_fiction, name="Literary Fiction"),
+    path('books/humor/', views.all_books_humor, name="Humor"),
+    path('books/motivational/', views.all_books_motivational, name="Motivational"),
+    path('books/historical_fiction/', views.all_books_historical_fiction, name="Historical Fiction"),
+    path('books/masnavyi/', views.all_books_masnavyi, name="fantasy"),
+
+
+
+
+
+
+
+
+
     path('books/<int:id>/', views.get_book_detail, name="books_url"),
+    path('books/<int:id>/update/', views.book_update, name="books_update"),
+    path('books/<int:id>/delete/', views.book_delete, name="books_delete"),
     path('add-books/', views.add_books, name="add_books"),
 ]
--- book/views.py
+++ book/views.py
@@ -1,10 +1,68 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404,redirect,reverse
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from . import models, forms
+from datetime import datetime, timedelta
+
+start_data = datetime.today() - timedelta(days=10)
 
 
 def all_books(request):
-    queryset = models.Book.objects.all()
+    queryset = models.Book.objects.order_by("-id")
+    return render(request, "books.html", {"books": queryset})
+
+
+def all_books_latest(request):
+    queryset = models.Book.objects.filter(created_date__gt=start_data).order_by("-id")
+    return render(request, "books.html", {"books": queryset})
+
+
+def all_books_drama(request):
+    queryset = models.Book.objects.filter(genre__in="Drama").order_by("-id")
+    return render(request, "books.html", {"books": queryset})
+
+
+def all_books_fantasy(request):
+    queryset = models.Book.objects.filter(genre__in="Fantasy").order_by("-id")
+    return render(request, "books.html", {"books": queryset})
+
+
+def all_books_thriller(request):
+    queryset = models.Book.objects.filter(genre__in="Thriller").order_by("-id")
+    return render(request, "books.html", {"books": queryset})
+
+
+def all_books_romance(request):
+    queryset = models.Book.objects.filter(genre__in="Romance").order_by("-id")
+    return render(request, "books.html", {"books": queryset})
+
+
+def all_books_westerns(request):
+    queryset = models.Book.objects.filter(genre__in="Westerns").order_by("-id")
+    return render(request, "books.html", {"books": queryset})
+
+
+def all_books_literary_fiction(request):
+    queryset = models.Book.objects.filter(genre__in="Literary Fiction").order_by("-id")
+    return render(request, "books.html", {"books": queryset})
+
+
+def all_books_humor(request):
+    queryset = models.Book.objects.filter(genre__in="Humor").order_by("-id")
+    return render(request, "books.html", {"books": queryset})
+
+
+def all_books_motivational(request):
+    queryset = models.Book.objects.filter(genre__in="Motivational").order_by("-id")
+    return render(request, "books.html", {"books": queryset})
+
+
+def all_books_historical_fiction(request):
+    queryset = models.Book.objects.filter(genre__in="Historical Fiction").order_by("-id")
+    return render(request, "books.html", {"books": queryset})
+
+
+def all_books_masnavyi(request):
+    queryset = models.Book.objects.filter(genre__in="Masnavyi").order_by("-id")
     return render(request, "books.html", {"books": queryset})
 
 
@@ -24,3 +82,22 @@ def add_books(request):
     else:
         form = forms.BookForm()
     return render(request, "add_books.html", {"form": form})
+
+
+def book_update(request, id):
+    book_object = get_object_or_404(models.Book, id=id)
+    if request.method == "POST":
+        form = forms.BookForm(instance=book_object, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("books_url:books_all_url"))
+    else:
+        form = forms.BookForm(instance=book_object)
+    return render(request, "book_update.html", {"form": form,
+                                                "object": book_object})
+
+
+def book_delete(request, id):
+    book_object = get_object_or_404(models.Book, id=id)
+    book_object.delete()
+    return redirect(reverse("books_url:books_all_url"))
